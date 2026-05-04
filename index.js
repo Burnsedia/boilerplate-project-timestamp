4a9b8daace3d071be24550d7d1698db64316ea92
Burnsedia <bburnsed1995@gmail.com>
2026-05-03 22:12:12 -0400

    building a microservice in express

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const { path } = require('express/lib/application');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,9 +22,12 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get(path: "/app/:date?", function getDate(params) {
+  res.json
+})
 
 
 // Listen on port set in environment variable or default to 3000
